a55755d8097c18bb5df3d83552d930ef5bf6ac2a
ShakyaNimesh <shakya.nimesh@gmail.com>
2016-09-22 09:22:35 +0545

    Override

---
+++ Hotel.java
@@ -0,0 +1,16 @@
+
+
+package Functionoverride;
+
+
+public class Hotel {
+    public void rooms(){
+    System.out.println("The hotel has 120 rooms.");
+}
+    public void food(){
+    System.out.println("The hotel has delicious food.");
+    }
+    public void Swimmingpool(){
+    System.out.println("The hotel has a Swimming pool");    
+    }
+}
---
+++ livingmain.java
@@ -0,0 +1,17 @@
+
+
+package Functionoverride;
+
+
+public class livingmain {
+    public static void main(String[] args){
+        Hotel a = new Hotel();
+        Hotel b= new Inn();
+        a.rooms();
+        a.food();
+        a.Swimmingpool();
+        b.rooms();
+        b.food();
+        b.Swimmingpool();
+    }
+}
